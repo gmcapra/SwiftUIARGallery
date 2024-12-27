d7174121067c7a5f59167020ab0a16933687d44b
Gianluca Capraro <gmcapra@umich.edu>
2024-12-27 08:30:09 -0700

    Initial Commit

---
+++ SwiftUIARGallery/SwiftUIARGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIARGalleryApp.swift
+//  SwiftUIARGallery
+//
+//  Created by Gianluca Capraro on 12/27/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIARGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
